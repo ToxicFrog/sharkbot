0d53dc4443c88ed238f9a93405d5a1dd9e93884f
Ben Kelly <bk@ancilla.ca>
2015-02-23 22:57:36 -0500

    Initial commit, basic eat command working

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject sharkybot2 "0.1.0-SNAPSHOT"
+  :description "Sends people for teeth lessons."
+  :url "irc://irc.freenode.net/gbchat"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [irclj "0.5.0-alpha4"]]
+  :main sharkybot2.core)
---
+++ src/sharkybot2/core.clj
@@ -0,0 +1,91 @@
+(ns sharkybot2.core
+  (:require [irclj.core :as irc]
+            [clojure.string :as string])
+  (:gen-class))
+
+; :users { ToxicFrog { :pronouns :m :shark-time (datetime) :spoilers :RoT }}
+; :spoilers { :LoLL 0 :RSURS 0 :RoT 3 }
+(def state (atom {:users {} :spoilers {}}))
+
+(def ^:dynamic *state* nil)
+(def ^:dynamic *irc* nil)
+(def ^:dynamic *msg* nil)
+
+(defn reply [& text]
+  (apply irc/reply *irc* *msg* text))
+
+; If the user has a spoiler level set, inc the spoiler refcount.
+; If this results in a lower spoiler level, report it.
+; If the user has an expired sharktimer, report that, clear the timer, and
+; save state.
+(defn on-join [nick user]
+  (prn (str nick "!" user) "JOIN")
+  *state*)
+
+; If the user has a spoiler level set, dec the spoiler refcount.
+; If this results in a higher spoiler level, report it.
+(defn on-quit [nick user]
+  (prn (str nick "!" user) "QUIT")
+  *state*)
+
+(defn eat-victim [victim]
+  (prn "EAT" victim)
+  (reply "ACTION drags" victim "beneath the waves, swimming around for a while before spitting their mangled remains back out.")
+  *state*)
+
+(defn set-pronouns [nick pronouns]
+  (prn "PRONOUNS" nick pronouns)
+  *state*)
+
+(defn set-spoilers [nick spoilers]
+  (prn "SPOILERS" nick spoilers))
+
+(defn to-command [text]
+  (let [nick (:nick @*irc*)]
+    (cond
+      (.startsWith text (str nick ", ")) (drop 1 (string/split text #"\s+" 4)) ; Sharky, command args
+      (.startsWith text "!") (-> text (subs 1) (string/split #"\s+" 3)) ; !command args
+      :else [nil ""])))
+
+; should respond to both "!command args" and "Sharky2, command args"
+; and also specific trigger text like "/me throws %s to the sharks"
+; and "/me sends %s for teeth lessons"
+(defn on-privmsg [nick user text]
+  (let [[command args] (take 2 (to-command text))
+        args (-> args (string/replace #"[.!,;]" " ") (string/trim))]
+    (case command
+      ("teeth" "eat") (eat-victim args)
+      "pronouns" (set-pronouns nick args)
+      "spoilers" (set-spoilers nick args)
+      *state*)))
+
+(defn on-irc [server msg]
+  (let [{text :text
+         target :target
+         nick :nick
+         user :user
+         command :command} msg]
+    (binding [*state* @state
+              *irc* server
+              *msg* msg]
+      (let [state'
+            (case command
+              "PRIVMSG" (on-privmsg nick user text)
+              "JOIN"    (on-join nick user)
+              "PART"    (on-quit nick user)
+              "QUIT"    (on-quit nick user)
+              *state*)]
+        (reset! state state')))))
+
+(def callbacks
+  {:privmsg on-irc
+   :join on-irc
+   :part on-irc
+   :quit on-irc})
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (let [server (irc/connect "irc.freenode.net" 6667 "Sharky2" :callbacks callbacks)]
+    (irc/join server "#gbchat")
+    (println "Done connecting to IRC.")))
