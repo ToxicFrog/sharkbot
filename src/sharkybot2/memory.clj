f8ebc7b4706ec4d630643f75540e72bb5219ed46
Ben Kelly <bk@ancilla.ca>
2015-03-13 19:11:50 -0400

    Replace !memories with no-arg !remember

--- src/sharkybot2/memory.clj
+++ src/sharkybot2/memory.clj
@@ -11,12 +11,14 @@
 (deftriggers remember [capa [key & vs]]
   "Remember a new fact, or show an old one."
   [(command "remember")]
-  (let [k (-> key .toLowerCase keyword)]
-    (if (empty? vs)
-      (reply (str key ":") (get-in @state [:memory k]))
-      (let [state' (assoc-in @state [:memory k] (string/join " " vs))]
-        (update-state state')
-        (reply "Remembered.")))))
+  (if (nil? key)
+    (reply "Memories:" (-> @state :memory keys pr-str))
+    (let [k (-> key .toLowerCase keyword)]
+      (if (empty? vs)
+        (reply (str key ":") (get-in @state [:memory k]))
+        (let [state' (assoc-in @state [:memory k] (string/join " " vs))]
+          (update-state state')
+          (reply "Remembered."))))))
 
 (deftriggers forget [capa [key]]
   "Forget a remembered fact."
@@ -25,9 +27,3 @@
         state' (update-in @state [:memory] #(dissoc % key))]
     (update-state state')
     (reply "Forgotten.")))
-
-(deftriggers memories [capa & _]
-  "List the shark's memories."
-  [(command "memories")]
-  (reply "Memories:" (-> @state :memory keys pr-str)))
-
