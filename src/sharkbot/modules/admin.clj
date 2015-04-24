663b502b4b00a1d3e903c9c32a168e9f69f1282e
Ben Kelly <bk@ancilla.ca>
2015-04-24 14:44:07 -0400

    Add 'raw' admin command.
    Sharky will send the line as entered to the IRC server.
    
    Note that to get it to do actions you need to be able to send \001
    characters from your client.

--- src/sharkbot/modules/admin.clj
+++ src/sharkbot/modules/admin.clj
@@ -2,6 +2,7 @@
   (:require
     [sharkbot.flags :refer [getopt]]
     [sharkbot.triggers :refer :all]
+    [sharkbot.irc :refer :all]
     ))
 
 (deftriggers die [capa _]
@@ -9,3 +10,10 @@
   [(command "die")]
   (when ((getopt :admin) capa)
     (System/exit 0)))
+
+(deftriggers raw [capa message]
+  "Emit a raw IRC command."
+  [(command "raw")]
+  (when ((getopt :admin) capa)
+    (let [message (apply str (interpose " " message))]
+      (send-irc message))))
