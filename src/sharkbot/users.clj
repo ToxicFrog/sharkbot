7721cc398fa7e2e41625165ecb6e68f1bdab9bfa
Ben Kelly <bk@ancilla.ca>
2016-01-22 19:51:32 -0500

    Remove a bunch of unused user functions

--- src/sharkbot/users.clj
+++ src/sharkbot/users.clj
@@ -5,30 +5,16 @@
             [clojure.string :as string]
             ))
 
-(defn canonical-name [nick]
-  (let [userinfo (-> @state :users (get nick))
-        aliased (->> @state :users (filter #(-> % second :aliases (contains? nick))) (map first))]
-    (cond
-      userinfo nick
-      (= 1 (count aliased)) (first aliased)
-      :else nick)))
-
-; Get info for a user.
-; If the user is known, either directly or by alias, return that user.
-; If there are multiple users known by this alias, returns nil after
-; reporting an error to IRC.
-; If the user is unknown, returns {}.
+; Get info for a user. Returns {} if the user is unknown.
 (defn get-user
   [nick]
-  (let [userinfo (-> @state :users (get nick))
-        aliased (->> @state :users (filter #(-> % second :aliases (contains? nick))) (map first))]
-    (cond
-      userinfo userinfo
-      (= 1 (count aliased)) (get-user (first aliased))
-      (< 1 (count aliased)) (do (reply "I know many people with that name:"
-                                       (string/join ", " aliased))
-                              nil)
-      :else {})))
+  (-> @state :users (get (string/lower-case nick) {})))
+
+; A user's *preferred name* is the name they have set as their :name. If they
+; haven't set a :name, it's the name they're currently wearing.
+(defn user-name [nick]
+  (or (:name (get-user nick))
+      nick))
 
 (def ^:private pronoun-map
   {:male #{"m" "male" "he" "him"}
@@ -49,11 +35,5 @@
       setting-to-gender
       {:male "his" :female "her" :neuter "their" :robot "its"}))
 
-; Returns the preferred name of the given user. If the user is unknown or
-; has no preferred name, returns nick.
-(defn user-name [nick]
-  (or (:name (get-user nick))
-      nick))
-
 (defn update-user [nick f]
-  (update-in @state [:users (canonical-name nick)] f))
+  (update-in @state [:users nick] f))
